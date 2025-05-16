d26e01411c439f0d8538d85eddc42a7bf5c33dde
sumit <sumit.yadav@bloomreach.com>
2025-05-16 11:15:51 +0530

    separate server and main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,40 +1,17 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/jackc/pgx/v5/pgxpool"
-
 	"github.com/sumityadav29/taskalley/config"
-	"github.com/sumityadav29/taskalley/internal/project"
-	"github.com/sumityadav29/taskalley/internal/task"
 )
 
 func main() {
-	cfg := config.Load()
+	server := NewServer()
 
-	dbPool, err := pgxpool.New(context.Background(), cfg.DatabaseUrl)
-	if err != nil {
-		log.Fatalf("failed to connect to db: %v", err)
-	}
-	defer dbPool.Close()
-
-	projectRepo := project.NewRepository(dbPool)
-	projectService := project.NewService(projectRepo)
-	projectHandler := project.NewHandler(projectService)
-
-	taskRepo := task.NewRepository(dbPool)
-	taskService := task.NewService(taskRepo)
-	taskHandler := task.NewHandler(taskService)
-
-	r := chi.NewRouter()
-	projectHandler.RegisterRoutes(r)
-	taskHandler.RegisterRoutes(r)
-	log.Printf("starting server on port %s", cfg.Port)
-	if err := http.ListenAndServe(":"+cfg.Port, r); err != nil {
+	log.Printf("starting server on port %s", config.Load().Port)
+	if err := http.ListenAndServe(":"+config.Load().Port, server); err != nil {
 		log.Fatalf("server error: %v", err)
 	}
 }
---
+++ cmd/server/server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"github.com/sumityadav29/taskalley/config"
+	"github.com/sumityadav29/taskalley/internal/project"
+	"github.com/sumityadav29/taskalley/internal/task"
+)
+
+func NewServer() http.Handler {
+	dbConfig := config.Load()
+
+	dbPool, err := pgxpool.New(context.Background(), dbConfig.DatabaseUrl)
+	if err != nil {
+		log.Fatalf("failed to connect to db: %v", err)
+	}
+	defer dbPool.Close()
+
+	projectRepo := project.NewRepository(dbPool)
+	projectService := project.NewService(projectRepo)
+	projectHandler := project.NewHandler(projectService)
+
+	taskRepo := task.NewRepository(dbPool)
+	taskService := task.NewService(taskRepo)
+	taskHandler := task.NewHandler(taskService)
+
+	r := chi.NewRouter()
+
+	projectHandler.RegisterRoutes(r)
+	taskHandler.RegisterRoutes(r)
+
+	return r
+}
